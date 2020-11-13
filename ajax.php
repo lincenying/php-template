8eb44913cc8fb731348563429c0431298b496506
林岑影 <lincenying@qq.com>
2020-11-13 17:03:38 +0800

    on 2020-11-13 17:03:38

--- ajax.php
+++ ajax.php
@@ -112,4 +112,10 @@ if ($action === 'upload') {
 
     $jsonStr = json_encode($return, JSON_UNESCAPED_UNICODE);
     echo $jsonStr;
+} else {
+    $return = [];
+    $return['code'] = 400;
+    $return['data'] = null;
+    $jsonStr = json_encode($return, JSON_UNESCAPED_UNICODE);
+    echo $jsonStr;
 }
